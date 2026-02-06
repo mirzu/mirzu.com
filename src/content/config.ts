8a64b946303b5f594a8aa6d7411fba07a729ff14
Mike Minecki <michal@mirzu.com>
2026-02-06 12:34:55 -0800

    added an image grid component and updated the blog and art collections to be able to use mdx

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
@@ -12,6 +14,7 @@ const blog = defineCollection({
 })
 
 const art = defineCollection({
+	loader: glob({ base: './src/content/art', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
