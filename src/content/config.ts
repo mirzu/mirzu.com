99561510fed5281a1ffa88fb28cdc0780a380e16
Mike Minecki <michal@mirzu.com>
2026-02-09 17:05:39 -0800

    refactored art to use a defined image path instead of a magic place to put images

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,13 +16,15 @@ const blog = defineCollection({
 const art = defineCollection({
 	loader: glob({ base: './src/content/art', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		coverImageCredit: z.string().optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			coverImageCredit: z.string().optional(),
+			coverImage: image(),
+		}),
 })
 
 export const collections = { blog, art }
